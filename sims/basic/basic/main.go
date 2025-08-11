3b4bfbb70ce9ea62e6f40d6c319fa54bc07a6525
Kai O'Reilly <koreilly5297@gmail.com>
2025-08-10 17:16:11 -0700

    basic: add command

---
+++ sims/basic/basic/main.go
@@ -0,0 +1,19 @@
+// Copyright (c) 2025, Kleroterio. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"cogentcore.org/core/core"
+	"github.com/kleroterio/abm/abm"
+	"github.com/kleroterio/abm/abmcore"
+	"github.com/kleroterio/abm/sims/basic"
+)
+
+func main() {
+	b := core.NewBody("Basic ABM Simulation")
+	sim := abm.NewSim[basic.Sim]()
+	abmcore.NewSim2D(b).SetSim(sim)
+	b.RunMainWindow()
+}
