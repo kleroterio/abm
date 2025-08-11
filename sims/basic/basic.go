31dd3b16b2be63e9fc16801a1fd1e7306a8da374
Kai O'Reilly <koreilly5297@gmail.com>
2025-08-10 17:14:44 -0700

    basic: start on basic sim

---
+++ sims/basic/basic.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2025, Kleroterio. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package basic implements a basic agent-based model simulation
+// for testing and demonstration purposes.
+package basic
+
+import "github.com/kleroterio/abm/abm"
+
+// Sim is a basic agent-based model simulation.
+type Sim struct {
+	abm.SimBase
+}
+
+func (s *Sim) Init() {
+	s.Agents = make([]abm.Agent, 100)
+	for i := range s.Agents {
+		s.Agents[i] = &abm.AgentBase{}
+	}
+
+	s.Base().Init()
+}
