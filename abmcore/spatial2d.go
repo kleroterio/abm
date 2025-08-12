2b93d840719674e6356f8cbded8f2c61040c4b9f
Kai O'Reilly <koreilly5297@gmail.com>
2025-08-12 15:56:16 -0700

    abmcore: start shifting to new plotcore.Editor based approach

--- abmcore/spatial2d.go
+++ abmcore/spatial2d.go
@@ -11,63 +11,65 @@ import (
 	"cogentcore.org/core/colors/cam/hct"
 	"cogentcore.org/core/styles"
 	"cogentcore.org/lab/plot"
-	"cogentcore.org/lab/plot/plots"
 	"cogentcore.org/lab/plotcore"
+	"cogentcore.org/lab/table"
 	"cogentcore.org/lab/tensor"
 	"github.com/kleroterio/abm/abm"
 )
 
 // Spatial2D is a 2d plot of a simulation based on the [abm.AgentBase.Position].
 type Spatial2D struct {
-	plotcore.Plot
+	plotcore.Editor
 
 	// Sim is the simulation that this 2D representation is based on.
 	Sim abm.Sim
+
+	// Table is the data table for plotting.
+	Table *table.Table `set:"-"`
 }
 
 func (sp *Spatial2D) Init() {
-	sp.Plot.Init()
+	sp.Editor.Init()
 	sp.Styler(func(s *styles.Style) {
 		s.Grow.Set(1, 1)
 	})
 
 	sp.Updater(func() {
-		sp.SetPlot(sp.MakePlot()) // TODO: more optimized updating?
+		if sp.Table == nil {
+			sp.MakeTable()
+		}
+		sp.UpdateTable()
 	})
 }
 
-// MakePlot creates the plot for the simulation.
-func (sp *Spatial2D) MakePlot() *plot.Plot {
-	pl := plot.New()
+// MakeTable creates the data table for plotting.
+func (sp *Spatial2D) MakeTable() {
+	sp.Table = table.New()
+	sp.Table.AddColumn("Spatial X", tensor.NewFloat32(1))
+	sp.Table.AddColumn("Spatial Y", tensor.NewFloat32(1))
+	sp.Table.AddColumn("Belief X", tensor.NewFloat32(1))
+	sp.Table.AddColumn("Belief Y", tensor.NewFloat32(1))
 
+	plot.Styler(sp.Table.Column("Spatial Y"), sp.colorStyler)
+	plot.Styler(sp.Table.Column("Belief Y"), sp.colorStyler)
+
+	sp.Editor.SetTable(sp.Table)
+}
+
+// UpdateTable updates the data table with the current agent data.
+func (sp *Spatial2D) UpdateTable() {
 	agents := sp.Sim.Base().Agents
-
-	n := len(agents)
-	spatialX := tensor.NewFloat32(n)
-	spatialY := tensor.NewFloat32(n)
-	beliefX := tensor.NewFloat32(n)
-	beliefY := tensor.NewFloat32(n)
+	sp.Table.SetNumRows(len(agents))
 
 	for i, a := range agents {
 		pos := a.Base().Position
-		spatialX.Set(pos.X, i)
-		spatialY.Set(pos.Y, i)
+		sp.Table.Column("Spatial X").SetFloat(float64(pos.X), i)
+		sp.Table.Column("Spatial Y").SetFloat(float64(pos.Y), i)
 
 		beliefs := a.Base().Beliefs
-		beliefX.Set(beliefs[0], i)
-		beliefY.Set(beliefs[1], i)
+		sp.Table.Column("Belief X").SetFloat(float64(beliefs[0]), i)
+		sp.Table.Column("Belief Y").SetFloat(float64(beliefs[1]), i)
 	}
-
-	plot.Styler(spatialY, sp.colorStyler)
-	plot.Styler(beliefY, sp.colorStyler)
-
-	data := plot.Data{
-		plot.X: spatialX,
-		plot.Y: spatialY,
-	}
-	plots.NewScatter(pl, data)
-
-	return pl
 }
 
 // colorStyler is a plot styler that styles points based on agent beliefs.
